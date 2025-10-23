f01ea6f2e233b94143d05248fddca0be9b7ac964
tobymoszer <tobymoszer@gmail.com>
2025-10-22 20:45:08 -0500

    Fix blur

--- src/client/java/tobymoszer/shulkerreader/screen/ShulkerReaderScreen.java
+++ src/client/java/tobymoszer/shulkerreader/screen/ShulkerReaderScreen.java
@@ -32,7 +32,6 @@ public class ShulkerReaderScreen extends HandledScreen<ShulkerReaderScreenHandle
 
 	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		renderBackground(context, mouseX, mouseY, delta);
 		super.render(context, mouseX, mouseY, delta);
 		drawMouseoverTooltip(context, mouseX, mouseY);
 	}

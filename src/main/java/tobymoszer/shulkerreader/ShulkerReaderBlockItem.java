4117aaabc55e14e69831699bef2f04421042d20d
tobymoszer <tobymoszer@gmail.com>
2026-07-02 02:18:43 -0500

    Prevent duplicate native placement sounds

--- src/main/java/tobymoszer/shulkerreader/ShulkerReaderBlockItem.java
+++ src/main/java/tobymoszer/shulkerreader/ShulkerReaderBlockItem.java
@@ -6,10 +6,15 @@ import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.fabric.api.networking.v1.context.PacketContext;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 import tobymoszer.shulkerreader.network.ShulkerReaderNetworking;
 
@@ -24,6 +29,18 @@ public final class ShulkerReaderBlockItem extends PolymerBlockItem implements Po
 			return this;
 		}
 		return super.getPolymerItem(stack, context);
+	}
+
+	@Override
+	public boolean isIgnoringBlockInteractionPlaySoundExceptedEntity(
+		BlockState state,
+		ServerPlayer player,
+		InteractionHand hand,
+		ItemStack stack,
+		ServerLevel world,
+		BlockHitResult hitResult
+	) {
+		return !ShulkerReaderNetworking.supportsNativeClient(player);
 	}
 
 	@Override
